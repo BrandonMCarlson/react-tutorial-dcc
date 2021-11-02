a86851169292f957e7ad678a7ebbcda20dea46bd
BrandonMCarlson <Brandoncarlson93bc@gmail.com>
2021-11-01 18:05:25 -0700

    book design is coming together. custum style implemented.

--- src/components/BookViewer/BookViewer.jsx
+++ src/components/BookViewer/BookViewer.jsx
@@ -1,20 +1,20 @@
 import React from "react";
+import Book from '../Book/Book';
 
 const BookViewer = (props) => {
     return (
     <div className="row">
+        <div className="col-md-4">
+            {/*Button here to move to the previous book viewed*/}
+            <button onClick={props.previousBook}>Previous Book</button>
+        </div>
     <div className="col-md-4">
-        {/*Button here to move to the previous book viewed*/}
-        <button onClick={props.previousBook}>Previous Book</button>
+        {/*Display book with cover here*/}
+        <Book book={props.book} />
     </div>
     <div className="col-md-4">
-    {/*Display book with cover here*/}
-    <h1>{props.book.title}</h1>
-    <h4>{props.book.author}</h4>
-    </div>
-    <div className="col-md-4">
-    {/*Button here to move to the next book viewed*/}
-    <button onClick={props.nextBook}>Next Book</button>
+        {/*Button here to move to the next book viewed*/}
+        <button onClick={props.nextBook}>Next Book</button>
     </div>
     </div>
 );
